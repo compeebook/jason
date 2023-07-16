b4f02d742e2422a431b8ac42d8e96be5ea5bf17b
compeebook <compeebook@gmail.com>
2023-07-16 15:09:06 -0700

    code for json

---
+++ myjavarscript.js
@@ -0,0 +1,21 @@
+ let user ={
+    name:"compeedddd",
+    age:7755,
+ getname(){
+   console.log(this.name,this.age +9)
+ }
+ }
+
+ let x =`{  
+"name":"dfdfd",
+ "age":77 }`
+ let jx ={
+   "name":"ccc",
+   "lang":["eng","thai","china"]
+
+ }
+ 
+ console.log(jx.lang[1])
+ console.log(user.age)
+ user.getname()
+ 
